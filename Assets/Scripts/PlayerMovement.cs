5e0585bb8b3d629623a90f457059fc29b66ca10f
xesos <jesus.gianzo@gmail.com>
2025-02-08 17:21:21 +0000

    Add collision to the player
    Also created prefab of the player so we can reuse it betsween scenes
    
    added input system now having just a log to mention if we click

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    Vector2 moveInput;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+}
