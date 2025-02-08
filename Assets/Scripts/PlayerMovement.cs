a143cdb0e68745fa53ee47b281183432afaa89d9
xesos <jesus.gianzo@gmail.com>
2025-02-08 17:30:19 +0000

    Add movement to the player
    Now it actually moves and responds to gravity and collisions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float speed = 10f;
     
     Vector2 moveInput;
-    
+    Rigidbody2D myRigidBody;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value) {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run(){
+        Vector2 playerVelocity = new Vector2(moveInput.x * speed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
     }
 }
