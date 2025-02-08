07329cbbd12a0d1b8d0f03a505c64c5ba6c4e6a4
xesos <jesus.gianzo@gmail.com>
2025-02-08 18:31:02 +0000

    Add climbing stairs
    Now we added a new tilemap for the stairs. We are able to climb stairs and modify the animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,32 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
-    
+    [SerializeField] float jumpSpeed = 20f;
+    [SerializeField] float climbSpeed = 10f;
+
+
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myCollider;
+    LayerMask myLayer;
+    float gravityAtStart;
     
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<CapsuleCollider2D>();
+        gravityAtStart = myRigidBody.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value) {
@@ -28,8 +40,41 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(moveInput);
     }
 
+    void OnJump(InputValue value) {
+        if(!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            return;
+        if (value.isPressed){
+            myRigidBody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     void Run(){
         Vector2 playerVelocity = new Vector2(moveInput.x * speed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning",playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite(){
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed){
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+
+    void ClimbLadder(){
+        if(!myCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))){
+            myRigidBody.gravityScale = gravityAtStart;
+            myAnimator.SetBool("isClimbing",false);
+            return;
+        }
+
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, moveInput.y * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+        myRigidBody.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing",playerHasVerticalSpeed);
+        
     }
 }
